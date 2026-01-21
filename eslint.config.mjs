d2f5d1dbedad58fc1c21374154289877887a1903
Morijellyfish <morijellyfish@gmail.com>
2026-01-21 21:03:35 +0900

    update : lint,prettier環境の整備

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,17 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
 
 export default withNuxt(
-  // Your custom configs here
+  // Prettierプラグインと設定を追加
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      ...prettierConfig.rules,
+      'prettier/prettier': 'error',
+    },
+  }
 )
