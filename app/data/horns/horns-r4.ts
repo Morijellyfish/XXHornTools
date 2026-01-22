22426cb21630fc1f125941b9b3e60942e7fabeee
Morijellyfish <morijellyfish@gmail.com>
2026-01-23 00:39:12 +0900

    add : Rare4,5の笛を追加

--- app/data/horns/horns-r4.ts
+++ app/data/horns/horns-r4.ts
@@ -1,5 +1,105 @@
 import type { Horn } from '~/types/horn'
+import { SharpnessColor } from '~/types/sharpness'
+import { Notes } from '~/types/notes'
 
 export const hornsR4: Horn[] = [
-  // レア度4の狩猟笛データをここに追加
+  {
+    name: '大蒼蛇笛ドスランポス6',
+    attack: 300,
+    defense: 0,
+    slots: 3,
+    affinity: 0,
+    notes: new Notes('紫', '黄', '赤'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 20 },
+      plus1: { color: SharpnessColor.White, length: 45 },
+      plus2: { color: SharpnessColor.White, length: 70 },
+    },
+  },
+  {
+    name: 'ビクトリードラム5',
+    attack: 310,
+    defense: 0,
+    slots: 1,
+    affinity: 0,
+    notes: new Notes('紫', '黄', '赤'),
+    sharpness: {
+      normal: { color: SharpnessColor.Blue, length: 40 },
+      plus1: { color: SharpnessColor.Blue, length: 65 },
+      plus2: { color: SharpnessColor.White, length: 20 },
+    },
+  },
+  {
+    name: 'リンフォルツァンド7',
+    attack: 320,
+    defense: 32,
+    slots: 1,
+    affinity: -5,
+    notes: new Notes('紫', '青', '赤'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 10 },
+      plus1: { color: SharpnessColor.Purple, length: 5 },
+      plus2: { color: SharpnessColor.Purple, length: 30 },
+    },
+  },
+  {
+    name: 'タイタンズ＝ロック5',
+    attack: 320,
+    defense: 15,
+    slots: 0,
+    affinity: 10,
+    element: {
+      type: '火',
+      value: 25,
+    },
+    notes: new Notes('紫', '青', '空'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 20 },
+      plus1: { color: SharpnessColor.White, length: 45 },
+      plus2: { color: SharpnessColor.Purple, length: 10 },
+    },
+  },
+  {
+    name: 'ケチャルファニワッパ6',
+    attack: 330,
+    defense: 0,
+    slots: 1,
+    affinity: 0,
+    element: {
+      type: '水',
+      value: 35,
+    },
+    notes: new Notes('白', '緑', '空'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 10 },
+      plus1: { color: SharpnessColor.Purple, length: 25 },
+      plus2: { color: SharpnessColor.Purple, length: 50 },
+    },
+  },
+  {
+    name: '土砂笙【厳ノ音】5',
+    attack: 340,
+    defense: 0,
+    slots: 2,
+    affinity: -25,
+    notes: new Notes('紫', '赤', '緑'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 10 },
+      plus1: { color: SharpnessColor.White, length: 35 },
+      plus2: { color: SharpnessColor.White, length: 60 },
+    },
+  },
+  {
+    name: '少し風化した狩猟笛7',
+    attack: 380,
+    defense: 0,
+    slots: 0,
+    affinity: -70,
+    notes: new Notes('白', '青', '赤'),
+    sharpness: {
+      normal: { color: SharpnessColor.Yellow, length: 190 },
+      plus1: { color: SharpnessColor.Blue, length: 5 },
+      plus2: { color: SharpnessColor.White, length: 10 },
+    },
+  },
 ]
--- app/data/horns/horns-r5.ts
+++ app/data/horns/horns-r5.ts
@@ -1,5 +1,104 @@
 import type { Horn } from '~/types/horn'
+import { SharpnessColor } from '~/types/sharpness'
+import { Notes } from '~/types/notes'
 
 export const hornsR5: Horn[] = [
-  // レア度5の狩猟笛データをここに追加
+  {
+    name: 'パラミティコール6',
+    attack: 280,
+    defense: 10,
+    slots: 2,
+    affinity: 0,
+    statusAilment: {
+      type: '麻痺',
+      value: 25,
+    },
+    notes: new Notes('紫', '赤', '空'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 30 },
+      plus1: { color: SharpnessColor.White, length: 55 },
+      plus2: { color: SharpnessColor.Purple, length: 20 },
+    },
+  },
+  {
+    name: 'グィロ＝フリギドゥス5',
+    attack: 290,
+    defense: 0,
+    slots: 0,
+    affinity: 25,
+    element: {
+      type: '氷',
+      value: 40,
+    },
+    notes: new Notes('紫', '空', '青'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 50 },
+      plus1: { color: SharpnessColor.Purple, length: 5 },
+      plus2: { color: SharpnessColor.Purple, length: 30 },
+    },
+  },
+  {
+    name: 'ケチャ・ウル・ハーラ6',
+    attack: 290,
+    defense: 0,
+    slots: 3,
+    affinity: 0,
+    element: {
+      type: '水',
+      value: 52,
+    },
+    notes: new Notes('紫', '緑', '空'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 40 },
+      plus1: { color: SharpnessColor.White, length: 65 },
+      plus2: { color: SharpnessColor.Purple, length: 10 },
+    },
+  },
+  {
+    name: '真氷鮫笛フェノミナル6',
+    attack: 330,
+    defense: 0,
+    slots: 0,
+    affinity: -10,
+    element: {
+      type: '氷',
+      value: 35,
+    },
+    notes: new Notes('紫', '赤', '黄'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 20 },
+      plus1: { color: SharpnessColor.White, length: 45 },
+      plus2: { color: SharpnessColor.Purple, length: 10 },
+    },
+  },
+  {
+    name: 'ヴォルガノアルピスタ5',
+    attack: 330,
+    defense: 0,
+    slots: 1,
+    affinity: -10,
+    element: {
+      type: '火',
+      value: 28,
+    },
+    notes: new Notes('紫', '黄', '赤'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 20 },
+      plus1: { color: SharpnessColor.White, length: 45 },
+      plus2: { color: SharpnessColor.Purple, length: 20 },
+    },
+  },
+  {
+    name: 'コンガコンガ6',
+    attack: 340,
+    defense: 0,
+    slots: 2,
+    affinity: 0,
+    notes: new Notes('紫', '青', '緑'),
+    sharpness: {
+      normal: { color: SharpnessColor.White, length: 10 },
+      plus1: { color: SharpnessColor.Purple, length: 5 },
+      plus2: { color: SharpnessColor.Purple, length: 30 },
+    },
+  },
 ]
